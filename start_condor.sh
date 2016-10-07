4405ecd5831536f881f88da7ef7bc8b6fb5b9724
Richard C <richard.calland@ipmu.jp>
2016-10-07 11:54:44 +0900

    Updated readme

--- start_condor.sh
+++ start_condor.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ansible nodes --u root --ask-pass -m shell -a "source ~condor/condor/condor.sh && condor_master && ps aux | grep condor"
+condor_cmd="source ~condor/condor/condor.sh && condor_master && ps aux | grep condor_master"
+
+ansible centos6_nodes --u root --ask-pass -m shell -a ${condor_cmd}
+ansible centos7_nodes --u root --ask-pass -m shell -a ${condor_cmd}
